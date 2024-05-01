bc91d81fa5671b7a8978c1a9031827a3a2a284e5
Zac <1271470655@qq.com>
2024-05-01 16:24:12 +0000

    化繁为简

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 /*imform*/
 console.log(" %c jquery.min.js %c v3.3.1 ", "color: #FFFFFF !important; background: #107a8b; padding:5px;", "background: #1c2b36; padding:5px;color:white !important");
 console.log(" %c NodeCursor.js %c v1.0.0 ", "color: #FFFFFF !important; background: #2cb978; padding:5px;", "background: #1c2b36; padding:5px;color:white !important");
-console.log(" %c parallax.min.js %c v3.1.0 ", "color: #FFFFFF !important; background: #83e85a; padding:5px;", "background: #1c2b36; padding:5px;color:white !important");
 console.log(" %c busuanzi.pure.mini.js %c v2.3.0 ", "color: #FFFFFF !important; background: #ff8a5c; padding:5px;", "background: #1c2b36; padding:5px;color:white !important");
-console.log(" %c Zac's personal homepage %c v2.1.0 ", "color: #FFFFFF !important; background: #FF6666; padding:5px;", "background: #1c2b36; padding:5px;color:white !important");
 console.log(` %c
 　　　　　　　　　　　　　＿＿＿　　 ~ヽ
 　　　　　　，‘　 ...::::::::::::::::::::::::::::｀丶
@@ -24,19 +22,6 @@ console.log(` %c
    ' 　 ｀ー- 、　｀　|＼/　 丶、　 　 |│　；
 　 　 　 　 　 ＼ 　_!　　　　　 ＼　　|│　;
 `,'font-family: "MS PGothic", "ＭＳ Ｐゴシック", "Trebuchet MS", Verdana, Futura, Arial, Helvetica, sans-serif;line-height: 1;font-size: 12pt;');
-
-
-/*background*/
-var scene = document.getElementById('header');
-	var parallaxInstance = new Parallax(scene, {
-    relativeInput: true,
-	  calibrateX: true,
-    scalarX: -10.0,
-    scalarY: -10.0,
-    clipRelativeInput: true,
-    frictionX: 1,
-    frictionY: 1
-})
 
 /*cursor*/
 if(window.innerWidth > 1200){
